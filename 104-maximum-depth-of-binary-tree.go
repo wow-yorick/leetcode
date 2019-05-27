9b6f235c474ca9cbd5becbf1b81a2d3a5006098c
yorick <yorick.yuan@verystar.cn>
2019-05-27 15:50:22 +0800

    maximum depth

--- 104-maximum-depth-of-binary-tree.go
+++ 104-maximum-depth-of-binary-tree.go
@@ -1,3 +1,11 @@
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
 /**
  * Definition for a binary tree node.
  * type TreeNode struct {
@@ -7,5 +15,15 @@
  * }
  */
 func maxDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	left := 1 + maxDepth(root.Left)
+	right := 1 + maxDepth(root.Right)
 
+	if left > right {
+		return left
+	} else {
+		return right
+	}
 }
