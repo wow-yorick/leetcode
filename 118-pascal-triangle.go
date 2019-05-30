13d850a42dbfc5daabc9086dcb0969ca68b9217f
yorick <yorick.yuan@verystar.cn>
2019-05-30 10:45:45 +0800

    config field

--- 118-pascal-triangle.go
+++ 118-pascal-triangle.go
@@ -1,5 +1,24 @@
 package main
 
 func generate(numRows int) [][]int {
-	return nil
+	if numRows == 0 {
+		return [][]int{}
+	}
+	if numRows == 1 {
+		return [][]int{{1}}
+	}
+	if numRows == 2 {
+		return [][]int{{1}, {1, 1}}
+	}
+	ret := generate(numRows - 1)
+	baseRow := ret[len(ret)-1]
+
+	tempRow := []int{1}
+	for i := 0; i+1 < len(baseRow); i++ {
+		tempRow = append(tempRow, baseRow[i]+baseRow[i+1])
+	}
+	tempRow = append(tempRow, 1)
+
+	ret = append(ret, tempRow)
+	return ret
 }
