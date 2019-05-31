48715a0c08c743a8deada9e2a6dfcdd66796990e
yorick <yorick.yuan@verystar.cn>
2019-05-31 11:08:10 +0800

    valid palindrome

---
+++ 125-valid-palindrome.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+	"unicode"
+)
+
+func main() {
+	isPalindrome("race a car")
+	//fmt.Printf("is eq %v", 'e' == 'a')
+}
+
+func isPalindrome(s string) bool {
+	if s == "" {
+		return true
+	}
+	s = strings.ToLower(s)
+	fmt.Printf("the s: %s \n", s)
+	i := 0
+	j := len(s) - 1
+	for i < j {
+		if !(unicode.IsLetter(rune(s[i])) || unicode.IsDigit(rune(s[i]))) {
+			i++
+			continue
+		}
+		if !(unicode.IsLetter(rune(s[j])) || unicode.IsDigit(rune(s[j]))) {
+			j--
+			continue
+		}
+
+		fmt.Printf("sj %v si %v \n", s[j], s[i])
+		if s[j] == s[i] {
+			i++
+			j--
+			continue
+		}
+		return false
+
+	}
+	return true
+}
