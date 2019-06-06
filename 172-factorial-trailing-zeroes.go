b12c61b0c4aa6b258886f86b6c1059cf1f15b611
yorick <yorick.yuan@verystar.cn>
2019-06-06 18:26:53 +0800

    exercises

--- 172-factorial-trailing-zeroes.go
+++ 172-factorial-trailing-zeroes.go
@@ -1,8 +1,24 @@
 package main
 
+import (
+	"fmt"
+	"reflect"
+)
+
 func main() {
-	ret := trailingZeroes(42)
-	println(ret)
+	// 	ret := trailingZeroes(42)
+	// 	println(ret)
+	str := "Hello,世界"
+	//方法一：格式化打印
+	for _, ch1 := range str {
+		fmt.Printf("%q \n", ch1) //单引号围绕的字符字面值，由go语法安全的转义
+	}
+	fmt.Println("==========>方法二\n")
+	//方法二：转化输出格式
+	for _, ch2 := range str {
+		fmt.Printf("%s", reflect.TypeOf(ch2))
+		fmt.Println(string(ch2))
+	}
 }
 
 func trailingZeroes(n int) int {
