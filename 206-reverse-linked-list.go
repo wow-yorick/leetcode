9a65d11764cabc46087c9ff8cb2f62aa0f5c5d55
yorick <yorick.yuan@verystar.cn>
2019-06-28 17:59:39 +0800

    feat:反转链表

--- 206-reverse-linked-list.go
+++ 206-reverse-linked-list.go
@@ -1,12 +1,44 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+)
+
 /**
  * Definition for singly-linked list.
- * type ListNode struct {
- *     Val int
- *     Next *ListNode
- * }
  */
 func reverseList(head *ListNode) *ListNode {
+	var prev *ListNode
+	var curr = head
 
+	for curr != nil {
+		nextTmp := curr.Next
+		curr.Next = prev
+		prev = curr
+		curr = nextTmp
+	}
+
+	return prev
+}
+
+func main() {
+
+	in := &ListNode{
+		Val:1,
+		Next:&ListNode{
+			Val:2,
+			Next:&ListNode{
+				Val:3,
+				Next:nil,
+			},
+		},
+	}
+
+	out := reverseList(in)
+
+	s,_ := json.Marshal(in)
+	d,_ := json.Marshal(out)
+
+	fmt.Printf("in: %s  out: %s", s, d)
 }
